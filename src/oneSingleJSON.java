8b4bbcd1e2763ffd53103b5aa686c39face67991
aappy <aparnachdhry@gmail.com>
2021-01-27 22:52:00 -0800

    added java obj which is coming from DB to JSON file

--- src/oneSingleJSON.java
+++ src/oneSingleJSON.java
@@ -7,15 +7,19 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-public class JsonTojava {
+import com.google.gson.Gson;
+public class oneSingleJSON {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, Exception, JsonMappingException, IOException {
-	
-		ArrayList<CustomerDetails> alist = new ArrayList<CustomerDetails>();
+
+        JSONArray js = new JSONArray();      
+		
+        ArrayList<CustomerDetails> alist = new ArrayList<CustomerDetails>();
 	
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = null; // to create connection
@@ -23,7 +27,7 @@ public class JsonTojava {
     	Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery("select * from CustomerInfo ");
 	
-      //where location = 'Asia' and PurchasedDate = curdate()
+      
         while(rs.next()) {
 //Create java object
         	CustomerDetails   obj = new CustomerDetails(); 	
@@ -40,12 +44,36 @@ public class JsonTojava {
         for (int i = 0; i < alist.size(); i++) {
 		
         ObjectMapper o = new ObjectMapper();
-        o.writeValue(new File("/Users/aparnachdhry/eclipse-workspace/JsonJava/customerInfo"+i+".json"), alist.get(i)); // passing obj valuue to the filee
-	    }
+       
+        o.writeValue(new File("/Users/aparnachdhry/eclipse-workspace/JsonJava/customerInfo"+i+".json"), alist.get(i)); // passing obj value to the file. This is file now
+	    
+        Gson g = new Gson();
         
+        String jsonString = g.toJson(alist.get(i));
+        js.add(jsonString);
         
-// JSON array
+        }
+      
+        JSONObject jo  = new JSONObject();
+        jo.put("data", js);
+      // System.out.println(jo.toString());
         
+        System.out.println(jo.toJSONString());
+       
         con.close();
+ /*        
+        
+// JSON array : We need to create JSON string from java object : for that - download gson dependency
+        Gson g = new Gson();
+        String jsonString = g.toJson();
+        
+        
+        JSONObject jo  = new JSONObject();
+        jo.put("data", "");
+      
+        JSONArray js = new JSONArray();      
+        js.add(); //it accepts string used gson dependency. 
+
+        */
 	}
 }
